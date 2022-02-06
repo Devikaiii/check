02aec571b5fa148f4e7d451285930677ba7168a8
Rithesh9747 <96607700+Rithesh9747@users.noreply.github.com>
2022-02-06 12:42:59 +0530

    Vb

--- plugins/inline.py
+++ plugins/inline.py
@@ -99,10 +99,11 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [
-        [
-            InlineKeyboardButton('Group', url='https://t.me/BlasterMovieszz')
-        ]
-        ]
+                    [
+                        InlineKeyboardButton('Group', url='https://t.me/BlasterMovieszz'),
+                        InlineKeyboardButton('Latest Movies', url='https://t.me/blasters_monthly'),
+                    ]
+                    ]
     return InlineKeyboardMarkup(buttons)
 
 

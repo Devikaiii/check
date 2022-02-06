c9c51b6e87cdfa1c17e15624513d47114ef3f629
Rithesh9747 <96607700+Rithesh9747@users.noreply.github.com>
2022-02-06 11:36:08 +0530

    Gh

--- plugins/inline.py
+++ plugins/inline.py
@@ -100,7 +100,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Group', url='https://t.me/BlasterMovieszz')
         ]
         ]
     return InlineKeyboardMarkup(buttons)

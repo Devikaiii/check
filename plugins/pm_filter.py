893788ca60dc4f3f328b0bba35209e0136fbf2e0
Rithesh9747 <96607700+Rithesh9747@users.noreply.github.com>
2022-02-06 13:35:18 +0530

    bn

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -349,6 +349,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f_caption
         if f_caption is None:
             f_caption = f"{files.file_name}"
+        buttons = [
+                    [
+                        InlineKeyboardButton('Group', url='https://t.me/BlasterMovieszz'),
+                        InlineKeyboardButton('Latest Movies', url='https://t.me/blasters_monthly'),
+                    ]
+                    ]
             
 
         try:
